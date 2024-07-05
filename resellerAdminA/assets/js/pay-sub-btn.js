cb5cb2ebfd40841a7548f96c12d2b3791eb1876e
zainab98alrez <zainab.98ta@gmail.com>
2024-07-05 13:35:55 +0300

    paybutton

---
+++ resellerAdminA/assets/js/pay-sub-btn.js
@@ -0,0 +1,20 @@
+﻿function paySubmit(id) {
+    var bl = document.getElementById(id);
+    alert("Hello");
+   
+
+    const radioBtnPay = document.querySelectorAll("input[name=option2]:checked");
+    
+    alert("zainab is here");
+    if (radioBtnPay.length <= 0) {
+        toastr.error("no no");
+       
+        bl.disabled = false;
+        return false;
+    }
+
+    bl.disabled = false;
+    toastr.success("all is done");
+
+    alert("if i have mistake i'll kill you");
+}
