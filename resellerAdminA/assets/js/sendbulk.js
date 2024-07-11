a1df95c2bbf201683a547e70759a98de3c57de83
ali-aljarrah <ali.jarrah.109@gmail.com>
2024-07-11 20:49:22 +0800

    Bulk SMS event

--- resellerAdminA/assets/js/sendbulk.js
+++ resellerAdminA/assets/js/sendbulk.js
@@ -1,11 +1,8 @@
-// Table datatable
 $('#latest-table').DataTable({
     "paging": false,
     "bInfo": false
 });
 
-// On keypress event to check the message length
-// and update the counter
 $('#txtMessage').on('keypress', function () {
     let len = this.value.length;
 
@@ -24,33 +21,31 @@ $('#txtMessage').on('keypress', function () {
     }
 });
 
-// Updating the selected files after picking a file
-// from the files tree
-$("[name='file-list-checkbox']").change(function () {
+$(".file-checkbox").change(function () {
     var value = $(this).val();
-    var filesTextArea = $('#selected-files').val();
+    var filesTextArea = $('#selectedFiles').val();
     var numbersCount = parseInt(value.match(/\|(.*?)\|/)[1]);
     var oldFilesCount = parseInt($('#files-count').text());
     var oldNumberCount = parseInt($('#number-count').text());
 
     if ($(this).is(':checked')) {
-        $('#selected-files').val(filesTextArea + value + ',');
+        $('#selectedFiles').val(filesTextArea + value + ',');
         $('#files-count').text(oldFilesCount + 1);
         $('#total_files').text(oldFilesCount + 1);
         $('#number-count').text(oldNumberCount + numbersCount);
         $('#total_recipients').text(oldNumberCount + numbersCount);
         $('#total_message').text(oldNumberCount + numbersCount);
     } else {
-        $('#selected-files').val(filesTextArea.replace(value + ',', ''));
+        $('#selectedFiles').val(filesTextArea.replace(value + ',', ''));
         $('#files-count').text(oldFilesCount - 1);
         $('#total_files').text(oldFilesCount - 1);
         $('#number-count').text(oldNumberCount - numbersCount);
         $('#total_recipients').text(oldNumberCount - numbersCount);
         $('#total_message').text(oldNumberCount - numbersCount);
     }
-});
+})
 
-// Templates placeholders to test the modal
+
 var templates = [
     {
         templateName: "Evacuation",
@@ -98,10 +93,8 @@ var templates = [
     }
 ];
 
-// Modal initialize
 var chooseTemplate = new bootstrap.Modal(document.getElementById("chooseTemplate"), {});
 
-// Updating the templates list in the modal and open it
 $('#chooseTemplateBtn').click(function (e) {
     e.preventDefault();
 
@@ -119,7 +112,7 @@ $('#chooseTemplateBtn').click(function (e) {
                     `+ templates[i].templateName + `
                 </td>
                 <td>
-                  <div class="p-2 black-color-1 fs-12 rounded bg-gray">  `+ templates[i].templateText + `</div>
+                    `+ templates[i].templateText + `
                 </td>
             </tr>
         `);
@@ -128,7 +121,6 @@ $('#chooseTemplateBtn').click(function (e) {
     chooseTemplate.show();
 });
 
-// Submit choose template event
 $('#chooseTemplateSubmitBtn').click(function (e) {
     e.preventDefault();
 
@@ -151,8 +143,6 @@ $('#chooseTemplateSubmitBtn').click(function (e) {
     }
 });
 
-// Updating the message body after choosing a
-// Personalize option
 $('#var_1').click(function (e) {
     e.preventDefault();
 
@@ -160,6 +150,7 @@ $('#var_1').click(function (e) {
 
     $('#txtMessage').val(oldText + ' [var1]');
 });
+
 $('#var_2').click(function (e) {
     e.preventDefault();
 
@@ -167,6 +158,7 @@ $('#var_2').click(function (e) {
 
     $('#txtMessage').val(oldText + ' [var2]');
 });
+
 $('#var_3').click(function (e) {
     e.preventDefault();
 
@@ -175,55 +167,50 @@ $('#var_3').click(function (e) {
     $('#txtMessage').val(oldText + ' [var3]');
 });
 
-// Submit send bulk SMS event
-$('#btnSubmit').click(function (e) {
-    e.preventDefault();
 
-    $(this).prop('disabled', true);
+function sendBulkSubmit(id) {
 
-    var txtSender = $('#txtSender').val();
-    var txtNumber = $('#txtNumber').val();
-    var cmbMessageType = $('#cmbMessageType').find(":selected").val();
-    var txtMessage = $('#txtMessage').val();
-    var selectedFiles = $('#selected-files').val();
+    var el = document.getElementById(id);
+    el.disabled = true;
+
+    var txtSender = document.getElementById("txtSender").value;
+    var e = document.getElementById("cmbMessageType");
+    var cmbMessageType = e.value;
+    var txtMessage = document.getElementById("txtMessage").value;
+    var selectedFiles = document.getElementById("selectedFiles").value;
 
     if (txtSender.length > 18 || txtSender.trim() == '') {
 
         toastr.error("Sender Name can't be empty and must be less than 18 characters!");
 
-        $(this).prop('disabled', false);
-
-        return;
+        el.disabled = false;
+        return false;
     }
 
     if (cmbMessageType == '') {
 
         toastr.error("Please select message type!");
 
-        $(this).prop('disabled', false);
-
-        return;
+        el.disabled = false;
+        return false;
     }
 
     if (selectedFiles.trim() == '') {
 
         toastr.error("Please select contact files!");
 
-        $(this).prop('disabled', false);
-
-        return;
+        el.disabled = false;
+        return false;
     }
 
     if (txtMessage.trim() == '') {
 
         toastr.error("Please enter message to send!");
 
-        $(this).prop('disabled', false);
-
-        return;
+        el.disabled = false;
+        return false;
     }
 
-    toastr.success("Message has been sent Successfully!");
-
-    $(this).prop('disabled', false);
-});
+    el.disabled = false;
+    __doPostBack(id, '');
+}
